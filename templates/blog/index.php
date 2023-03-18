5d82100adfe1d6215667bb9c70a4d60dc6b492d0
rafl00 <rafalpopiolek0024@gmail.com>
2023-03-18 23:59:44 +0100

    Hide create button for unauthenticated users

--- templates/blog/index.php
+++ templates/blog/index.php
@@ -1,11 +1,13 @@
 <h1>Blogs</h1>
 
 <div class="container">
-    <div class="d-flex justify-content-end">
-        <a href="/blog/?action=create">
-            <button class="btn btn-primary">Create</button>
-        </a>
-    </div>
+    <?php if(isAuth()): ?>
+        <div class="d-flex justify-content-end">
+            <a href="/blog/?action=create">
+                <button class="btn btn-primary">Create</button>
+            </a>
+        </div>
+    <?php endif; ?>
 
     <div class="mt-3">
         <table id="blogsTable">
